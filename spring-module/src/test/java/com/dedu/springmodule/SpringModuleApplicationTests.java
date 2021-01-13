3b300025cbebc87c31c744b94011436b08a57091
dedu18 <swpuxzh@163.com>
2021-01-13 17:06:37 +0800

    【新增】spring-module

---
+++ spring-module/src/test/java/com/dedu/springmodule/SpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedu.springmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
